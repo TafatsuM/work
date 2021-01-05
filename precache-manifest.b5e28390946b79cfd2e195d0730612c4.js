e1bef3bff24b019a9792b6e76e827baccddbec82
TafatsuM <turkdemirmustafa@gmail.com>
2021-01-05 22:07:14 +0300

    Updates

---
+++ precache-manifest.b5e28390946b79cfd2e195d0730612c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14c1dc6b10ae839e8058d16040f6927e",
+    "url": "/work/index.html"
+  },
+  {
+    "revision": "f46c0f0a4f150d13a3bd",
+    "url": "/work/static/css/main.6dbb99a6.chunk.css"
+  },
+  {
+    "revision": "7a41d20382ab058b471d",
+    "url": "/work/static/js/2.a64e8262.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/work/static/js/2.a64e8262.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f46c0f0a4f150d13a3bd",
+    "url": "/work/static/js/main.d5fd4e3c.chunk.js"
+  },
+  {
+    "revision": "f311ae9087f233303650",
+    "url": "/work/static/js/runtime-main.49dcbb09.js"
+  },
+  {
+    "revision": "209a89333f9c896dcc8f292b8afd0378",
+    "url": "/work/static/media/background_search_desktop.209a8933.jpg"
+  },
+  {
+    "revision": "8ffaa796a28a8a6aa5086c87bf9809c5",
+    "url": "/work/static/media/background_search_mobile.8ffaa796.jpg"
+  }
+]);
